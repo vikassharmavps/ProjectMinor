2f05ac1e17dab99d61f9cd8bebb45c54e17335a1
vikassharmavps <40141567+vikassharmavps@users.noreply.github.com>
2018-07-23 15:56:13 +0530

    Add files via upload

---
+++ Progress2/script.js
@@ -0,0 +1,29 @@
+window.onscroll = function() {myFunction()};
+window.onscroll = function() {scrollFunction()};
+
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+/*function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topBtn").style.display = "block";
+    } else {
+        document.getElementById("topBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}*/
